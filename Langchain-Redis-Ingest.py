84a7b7678e3d265728ae99f742cf5da943c8be96
sauagarwa <sauagarw@redhat.com>
2024-04-22 12:53:17 -0400

    Add support of PGVector

--- Langchain-Redis-Ingest.py
+++ Langchain-Redis-Ingest.py
@@ -17,12 +17,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.vectorstores.redis import Redis
 import os
+from vector_db.db_provider_factory import DBFactory
 
 # In[17]:
 
-redis_url = os.getenv('REDIS_URL')
-index_name = "docs"
-
+type = os.getenv('DB_TYPE') if os.getenv('DB_TYPE') else "REDIS"
+db_provider = DBFactory().create_db_provider(type)
 
 # #### Imports
 
@@ -34,16 +34,11 @@ index_name = "docs"
 
 # In[19]:
 
-
 pdf_folder_path = 'rhods-doc'
-
 loader = PyPDFDirectoryLoader(pdf_folder_path)
 docs = loader.load()
 
-
 # #### Split documents into chunks with some overlap
-
-
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024,
                                                chunk_overlap=40)
@@ -54,72 +49,37 @@ all_splits = text_splitter.split_documents(docs)
 
 # In[21]:
 
-
-embeddings = HuggingFaceEmbeddings()
-rds = Redis.from_documents(all_splits,
-                           embeddings,
-                           redis_url=redis_url,
-                           index_name=index_name)
-
-
-# #### Write the schema to a yaml file to be able to open the index later on
-
-# In[22]:
-
-print("Creating schema...")
-rds.write_schema("redis_schema.yaml")
-
+db_provider.add_documents(all_splits)
 
 # ## Ingesting new documents
-
 # #### Example with Web pages
 
 # In[23]:
 
-
 from langchain.document_loaders import WebBaseLoader
-
 
 # In[24]:
 
-
 loader = WebBaseLoader(["https://ai-on-openshift.io/getting-started/openshift/",
                         "https://ai-on-openshift.io/getting-started/opendatahub/",
-                        "https://ai-on-openshift.io/getting-started/openshift-data-science/",
-                        "https://ai-on-openshift.io/odh-rhods/configuration/",
-                        "https://ai-on-openshift.io/odh-rhods/custom-notebooks/",
-                        "https://ai-on-openshift.io/odh-rhods/nvidia-gpus/",
-                        "https://ai-on-openshift.io/odh-rhods/custom-runtime-triton/",
-                        "https://ai-on-openshift.io/odh-rhods/openshift-group-management/",
-                        "https://ai-on-openshift.io/tools-and-applications/minio/minio/"
+                        "https://ai-on-openshift.io/getting-started/openshift-ai/",
+                        "https://ai-on-openshift.io/odh-rhoai/configuration/",
+                        "https://ai-on-openshift.io/odh-rhoai/custom-notebooks/",
+                        "https://ai-on-openshift.io/odh-rhoai/nvidia-gpus/",
+                        "https://ai-on-openshift.io/odh-rhoai/custom-runtime-triton/",
+                        "https://ai-on-openshift.io/odh-rhoai/openshift-group-management/",
+                        "https://ai-on-openshift.io/tools-and-applications/minio/minio/",
+                        "https://access.redhat.com/articles/7047935",
+                        "https://access.redhat.com/articles/rhoai-supported-configs",
                        ])
 
-
 # In[25]:
-
-
 data = loader.load()
 
-
 # In[26]:
-
-
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024,
                                                chunk_overlap=40)
 all_splits = text_splitter.split_documents(data)
 
-
 # In[27]:
-
-
-embeddings = HuggingFaceEmbeddings()
-rds = Redis.from_existing_index(embeddings,
-                                redis_url=redis_url,
-                                index_name=index_name,
-                                schema="redis_schema.yaml")
-
-
-# In[28]:
-
-
-rds.add_documents(all_splits)
+db_provider.add_documents(all_splits)
